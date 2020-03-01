0beec81db6be073761d71b9495be51ceb66f7113
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-03-01 03:07:55 +0200

    Change icon class (FontAwesome update)

--- Module.php
+++ Module.php
@@ -96,7 +96,7 @@ class Module extends BaseModule
         $items = [
             'label' => $this->name,
             'url' => [$this->routePrefix . '/'. $this->id],
-            'icon' => 'fa-bullhorn',
+            'icon' => 'fa fa-fw fa-feather-alt',
             'active' => in_array(\Yii::$app->controller->module->id, [$this->id])
         ];
         return $items;
